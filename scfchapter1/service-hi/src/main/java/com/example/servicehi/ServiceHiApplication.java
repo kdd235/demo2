75a0846b408a81f9244db3321df8f0260f841805
1834908032@qq.com <1834908032@qq.com>
2018-09-11 17:53:37 +0800

    Initial commit

---
+++ scfchapter1/service-hi/src/main/java/com/example/servicehi/ServiceHiApplication.java
@@ -0,0 +1,91 @@
+package com.example.servicehi;
+
+import com.example.servicehi.impl.AquiredLockWorker;
+import com.example.servicehi.util.RedisLocker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceHiApplication.class, args );
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+
+    @Autowired
+    RedisLocker distributedLocker;
+    @RequestMapping(value = "/redlock")
+    public String testRedlock() throws Exception{
+
+        CountDownLatch startSignal = new CountDownLatch(1);
+        CountDownLatch doneSignal = new CountDownLatch(5);
+        for (int i = 0; i < 5; ++i) { // create and start threads
+            new Thread(new Worker(startSignal, doneSignal)).start();
+        }
+        startSignal.countDown(); // let all threads proceed
+        doneSignal.await();
+        System.out.println("All processors done. Shutdown connection");
+        return "redlock";
+    }
+
+    class Worker implements Runnable {
+        private final CountDownLatch startSignal;
+        private final CountDownLatch doneSignal;
+
+        Worker(CountDownLatch startSignal, CountDownLatch doneSignal) {
+            this.startSignal = startSignal;
+            this.doneSignal = doneSignal;
+        }
+
+        public void run() {
+            try {
+                startSignal.await();
+                distributedLocker.lock("test",new AquiredLockWorker<Object>() {
+
+                    @Override
+                    public Object invokeAfterLockAquire() {
+                        doTask();
+                        return null;
+                    }
+
+                });
+            }catch (Exception e){
+
+            }
+        }
+
+        void doTask() {
+            System.out.println(Thread.currentThread().getName() + " start");
+            Random random = new Random();
+            int _int = random.nextInt(200);
+            System.out.println(Thread.currentThread().getName() + " sleep " + _int + "millis");
+            try {
+                Thread.sleep(_int);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " end");
+            doneSignal.countDown();
+        }
+    }
+}
